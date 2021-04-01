febb0a20a427cfa46ee667dcfcbca5c9c28733f5
mdelgado509 <mdelgado509@gmail.com>
2021-04-01 14:18:02 -0400

    Test API change password functionality via curl script

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -1,4 +1,4 @@
-# EMAIL=miked@gmail.com PASSWORD=bananamango sh curl-scripts/auth/sign-in.sh
+# EMAIL=mdelgado509@gmail.com PASSWORD=a sh curl-scripts/auth/sign-in.sh
 
 curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
   --include \
--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,4 +1,4 @@
-# EMAIL=miked@gmail.com PASSWORD=bananamango sh curl-scripts/auth/sign-up.sh
+# EMAIL=mdelgado509@gmail.com PASSWORD=a sh curl-scripts/auth/sign-up.sh
 
 curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
   --include \
