7e68406f4e5c3a3e4b804914af74ec91a7ce77aa
mdelgado509 <mdelgado509@gmail.com>
2021-04-01 13:29:34 -0400

    Verify API sign in request

---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,14 @@
+# EMAIL=miked@gmail.com PASSWORD=bananamango sh curl-scripts/auth/sign-in.sh
+
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
